0c3eb172605b026992c92ed49a6202a11c4340f7
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-02-05 17:05:52 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -23,7 +23,7 @@ var firebaseConfig = {
   function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
          Room_names = childKey;
         console.log(Room_names);
-        row="<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+        row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
         document.getElementById("output").innerHTML+=row;
         });});}
   getData();
